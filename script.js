4576c9a6cac4d8288156a63e8a4247364be873d8
Md Anwar Alam <113902153+programer12345anwar@users.noreply.github.com>
2025-04-26 16:48:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let input=document.getElementById("fname");
+// Add a blur event listener
+input.addEventListener('blur', function(){
+	input.value=input.value.toUpperCase();
+})
